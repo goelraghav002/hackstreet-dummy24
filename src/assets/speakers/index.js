084381930ad9fe859cd59b4b6ccb1a6401f881f3
Vanshul Agarwal <vanshulagarwal2004@gmail.com>
2024-01-08 01:22:12 +0530

    added speakers

--- src/assets/speakers/index.js
+++ src/assets/speakers/index.js
@@ -1,4 +1,22 @@
-import rohith from "./rohith.jpeg";
-import vaibbhav from "./vaibbhav.jpeg";
+import Dr_Shruti_Kalra from "./Dr_Shruti_Kalra.jpg";
+import Ayush_Kumar from "./Ayush_Kumar.jpeg";
+import Priyanshi_Rai from "./Priyanshi_Rai.jpeg";
+import Abiha_Naqvi from "./Abiha_Naqvi.jpg";
+import Ansh from "./Ansh.jpg";
+import Aryan_Gupta from "./Aryan_Gupta_CoverPhoto.jpg";
+import Aryesh_Srivastava from "./Aryesh Srivastava.jpeg";
+import Avisha_Goyal from "./Avisha_Goyal.jpg";
+import Devang from "./Devang.jpg";
+import Ishika from "./Ishika.jpg";
+import Komal_Paliwal from "./Komal_Paliwal.jpg";
+import Manan from "./Manan.jpg";
+import Megha from "./Megha.png";
+import Nikita_Bansal from "./Nikita_Bansal.jpg";
+import Nitya_Kapur from "./Nitya_Kapur.jpg";
+import Raghav_Goel from "./Raghav_Goel.png";
+import Saara_Tyagi from "./Saara_Tyagi.jpg";
+import Shaheera_Fatima from "./Shaheera_Fatima.jpg";
+import Tanish_Goyal from "./Tanish_Goyal.jpg";
+import Tanya_Gupta from "./Tanya_Gupta.jpg";
 
-export { rohith, vaibbhav };
+export { Dr_Shruti_Kalra, Ayush_Kumar, Priyanshi_Rai, Abiha_Naqvi, Ansh, Aryan_Gupta, Aryesh_Srivastava, Avisha_Goyal, Devang, Ishika, Komal_Paliwal, Manan, Megha, Nikita_Bansal, Nitya_Kapur, Raghav_Goel, Saara_Tyagi, Shaheera_Fatima, Tanish_Goyal, Tanya_Gupta }
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,5 @@
 import { ieee, jiit, amd, aujus } from "../assets/organizers";
-import { rohith, vaibbhav } from "../assets/speakers";
-
+import { Dr_Shruti_Kalra, Ayush_Kumar, Priyanshi_Rai, Abiha_Naqvi, Ansh, Aryan_Gupta, Aryesh_Srivastava, Avisha_Goyal, Devang, Ishika, Komal_Paliwal, Manan, Megha, Nikita_Bansal, Nitya_Kapur, Raghav_Goel, Saara_Tyagi, Shaheera_Fatima, Tanish_Goyal, Tanya_Gupta } from "../assets/speakers";
 export const prizes = [
   {
     icon: '<i class="first fa-solid fa-trophy fa-4x"></i>',
@@ -37,14 +36,104 @@ export const organizers = [
 
 export const speakers = [
   {
-    name: "Rohith Gopala Krishna",
-    designation: "Country Head - AMD",
-    img: rohith,
+    name: "Dr. Shruti Kalra",
+    designation: "FACULTY COORDINATOR",
+    img: Dr_Shruti_Kalra,
   },
   {
-    name: "Vaibbhav Mishrra",
-    designation: "Director & Co-Founder - AUJUS Technologies",
-    img: vaibbhav,
+    name: "Ayush Kumar",
+    designation: "GitHub Campus Expert",
+    img: Ayush_Kumar,
+  },
+  {
+    name: "Priyanshi Rai",
+    designation: "GitHub Campus Expert",
+    img: Priyanshi_Rai,
+  },
+  {
+    name: "Aryan Gupta",
+    designation: "Chairperson",
+    img: Aryan_Gupta,
+  },
+  {
+    name: "Ansh Mishra",
+    designation: "Vice-Chaiperson",
+    img: Ansh,
+  },
+  {
+    name: "Aryesh Srivastava",
+    designation: "Organising Secretary",
+    img: Aryesh_Srivastava,
+  },
+  {
+    name: "Tanya Gupta",
+    designation: "Treasurer",
+    img: Tanya_Gupta,
+  },
+  {
+    name: "Avisha Goyal",
+    designation: "Tech Head CSE",
+    img: Avisha_Goyal,
+  },
+  {
+    name: "Nikita Bansal",
+    designation: "Strategic Head",
+    img: Nikita_Bansal,
+  },
+  {
+    name: "Manan Jain",
+    designation: "Digital Head",
+    img: Manan,
+  },
+  {
+    name: "Nitya Kapur",
+    designation: "Marketing Head",
+    img: Nitya_Kapur,
+  },
+  {
+    name: "Tanish Goyal",
+    designation: "Management Head",
+    img: Tanish_Goyal,
+  },
+  {
+    name: "Saara Tyagi",
+    designation: "Creative Head",
+    img: Saara_Tyagi,
+  },
+  {
+    name: "Abiha Naqvi",
+    designation: "WIE HEAD",
+    img: Abiha_Naqvi,
+  },
+  {
+    name: "Komal Paliwal",
+    designation: "PR HEAD",
+    img: Komal_Paliwal,
+  },
+  {
+    name: "Raghav Goel",
+    designation: "Webmaster",
+    img: Raghav_Goel,
+  },
+  {
+    name: "Ishika Shukla",
+    designation: "Head Of Operations",
+    img: Ishika,
+  },
+  {
+    name: "Devang Sharma",
+    designation: "Tech Head ECE",
+    img: Devang,
+  },
+  {
+    name: "Megha Chauhan",
+    designation: "Cinematography Head",
+    img: Megha,
+  },
+  {
+    name: "Shaheera Fatima",
+    designation: "128 Head",
+    img: Shaheera_Fatima,
   },
 ];
 
